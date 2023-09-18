7f661df53846ccffafdf0ee1a75e396bc49d1e8d
aantoin <aantoin@g.clemson.edu>
2023-09-17 20:32:42 -0400

    Add Todo App to project page

--- src/components/ProjectsPageComponent.vue
+++ src/components/ProjectsPageComponent.vue
@@ -3,9 +3,11 @@
     <h1 class="text-4xl border-b-slate-600 border-b-4 mb-2">Projects</h1>
     <div v-for="(project, index) in projects" :key="'project-' + index" class="border-b-2">
       <h2 class="text-3xl pb-2">{{ project.name }}</h2>
-      <a class="inline-block my-1 mr-1 p-1 border-2 border-slate-600 hover:bg-slate-600 hover:text-white rounded-xl cursor-pointer"
+      <a v-if="project.live"
+        class="inline-block my-1 mr-1 p-1 border-2 border-slate-600 hover:bg-slate-600 hover:text-white rounded-xl cursor-pointer"
         :href="project.live" target="_blank">Live</a>
-      <a class="inline-block my-1 p-1 border-2 border-slate-600 hover:bg-slate-600 hover:text-white rounded-xl cursor-pointer"
+      <a v-if="project.source"
+        class="inline-block my-1 p-1 border-2 border-slate-600 hover:bg-slate-600 hover:text-white rounded-xl cursor-pointer"
         :href="project.source" target="_blank">Source</a>
       <p>{{ project.description }}</p>
     </div>
@@ -21,6 +23,12 @@ defineProps<{
 }>()
 
 const projects = ref([
+  {
+    name: 'Todo App',
+    live: '',
+    source: 'https://github.com/aantoin/Todo_App',
+    description: 'A REST app made with Django, Django REST Framework, Vue JS, and Tailwind CSS.'
+  },
   {
     name: 'ArthurFAntoine.com',
     live: 'https://arthurfantoine.com',
